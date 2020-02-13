37c1a99737e5db7cee8ba7b129ce863109307024
glenn <glenn@simple.info.com>
2020-02-13 18:14:29 +0800

    Build to upload /build path.

---
+++ build/precache-manifest.a3fb2b9b6686eb5f642b1d2f986d100f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31a68bd87daba9cc52f38da9ad1a24d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8de81aade1a45b8de1",
+    "url": "/static/css/main.95bc470f.chunk.css"
+  },
+  {
+    "revision": "fa0eb501af34c8648d9b",
+    "url": "/static/js/2.0a701b68.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0a701b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8de81aade1a45b8de1",
+    "url": "/static/js/main.23ffb57e.chunk.js"
+  },
+  {
+    "revision": "c26ef94ce5349e6b5cae",
+    "url": "/static/js/runtime-main.31bd3342.js"
+  }
+]);
